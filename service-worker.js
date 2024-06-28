7d941c8441db7a492a04496a26af0faa59dc9281
Hendryksch <150305266+Hendryksch@users.noreply.github.com>
2024-06-28 06:51:47 +0200

    style update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-cache-v1.0.5'; // Aktualisiere die Versionsnummer bei Änderungen
+const CACHE_NAME = 'my-cache-v1.0.5b'; // Aktualisiere die Versionsnummer bei Änderungen
 const urlsToCache = [
   '/',
   '/index.html',
